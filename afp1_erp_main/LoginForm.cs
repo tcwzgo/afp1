f45f755b8de4ea58ea3adc9e87d13ad899d0870d
Sipos Levente <d985et@std.uni-eszterhazy.hu>
2021-10-02 13:22:16 +0200

    LoginForm simplified
    deleted several rows and deleted all the errorProvider

--- afp1_erp_main/LoginForm.cs
+++ afp1_erp_main/LoginForm.cs
@@ -20,103 +20,24 @@ namespace afp1_erp_main
         }
         private void btn_login_Click(object sender, EventArgs e)
         {
-            if(tb_uName.Text == string.Empty)
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Please enter your username!");
-                
-            }
-            if (tb_passwd.Text == string.Empty)
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_passwd, "Please enter your password");
-                
-            }
-            if (tb_uName.Text == string.Empty && tb_passwd.Text == string.Empty)
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Please enter your username!");
-                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
-            }
-            if (tb_uName.Text == string.Empty && tb_passwd.Text != "admin")
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Please enter your username!");
-                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
-            }
-            if (tb_passwd.Text == string.Empty && tb_uName.Text != "admin")
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Incorrect username!");
-                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
-            }
-            if (tb_uName.Text != "admin")
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Incorrect username!");
-                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
-            }
-            if (tb_passwd.Text != "admin")
-            {
-                tb_uName.Focus();
-                errorProvider.SetError(tb_passwd, "Incorrect password!");
-                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
-            }
             if (tb_uName.Text == "admin" && tb_passwd.Text == "admin")
             {
                 Thread t = new Thread(Run);
                 t.Start();
                 Close();
             }
-            
+            else
+            {
+                tb_uName.Focus();
+                MessageBox.Show("Incorrect data", "Error", MessageBoxButtons.OK);
+                tb_uName.Clear();
+                tb_passwd.Clear();
+            }
         }
         private void Run()
         {
             Thread.Sleep(500);
             Application.Run(new MainForm());
-        
         }
-
-        /*private void tb_uName_Validating(object sender, CancelEventArgs e)
-        {
-            if (string.IsNullOrEmpty(tb_uName.Text))
-            {
-                e.Cancel = true;
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Please enter your username!");
-            }
-            else if(tb_uName.Text != "admin")
-            {
-                e.Cancel = true;
-                tb_uName.Focus();
-                errorProvider.SetError(tb_uName, "Incorrect username!");
-            }
-            else
-            {
-                e.Cancel = true;
-                errorProvider.SetError(tb_uName, null);
-            }
-        }*/
-
-        /*private void tb_passwd_Validating(object sender, CancelEventArgs e)
-        {
-            if (string.IsNullOrEmpty(tb_passwd.Text))
-            {
-                e.Cancel = true;
-                tb_uName.Focus();
-                errorProvider.SetError(tb_passwd, "Please enter your password");
-            }
-            else if (tb_passwd.Text != "admin")
-            {
-                e.Cancel = true;
-                tb_uName.Focus();
-                errorProvider.SetError(tb_passwd, "Incorrect username!");
-            }
-            else
-            {
-                e.Cancel = false;
-                errorProvider.SetError(tb_passwd, null);
-            }
-        }*/
     }
 }
